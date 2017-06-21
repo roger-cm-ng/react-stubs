f33a54e4f186a0aefd553656acde994b738491cb
3PAUS44\Roger Ng <roger.ng@3plearning.com>
2017-06-21 19:41:55 +1000

    stateless

--- templates/js-content-main-stateless.js
+++ templates/js-content-main-stateless.js
@@ -3,17 +3,18 @@ require('babel-register')();
 module.exports = function(compTitle, fileName) {
   return `import React from 'react';
 import styleable from 'react-styleable';
+import PropTypes from 'prop-types';
 import '../../styles/core.scss';
 import css from './${fileName}.scss';
 
-const ${compTitle} = ({ options }) => (
+const ${compTitle} = ({ str = 'state less component' }) => (
   <div className={css.component}>
-		<h1>Component</h1>
+		<h1>{str}</h1>
   </div>
 );
 
 ${compTitle}.propTypes = {
-
+	str: PropTypes.string
 };
 
 export default styleable(css)(${compTitle});
