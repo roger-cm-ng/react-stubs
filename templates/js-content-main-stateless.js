f6904785ae2513c932bb17b4fcea8553f78bce1a
3PAUS44\Roger Ng <roger.ng@3plearning.com>
2017-06-21 19:37:15 +1000

    stateless

--- templates/js-content-main-stateless.js
+++ templates/js-content-main-stateless.js
@@ -6,9 +6,9 @@ import styleable from 'react-styleable';
 import '../../styles/core.scss';
 import css from './${fileName}.scss';
 
-const ${compTitle} = ({}) => (
+const ${compTitle} = ({ options }) => (
   <div className={css.component}>
-
+		<h1>Component</h1>
   </div>
 );
 
